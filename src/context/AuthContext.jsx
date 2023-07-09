7b9af3e8720b93b53b8878ba6cba88adc9e29d4b
darshan15062002 <darshanjain15062002@gmail.com>
2023-07-09 19:52:38 +0530

    api integration

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,18 +1,23 @@
 import { onAuthStateChanged } from "firebase/auth";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import { auth } from "../firebase";
 
 export const AuthContext = createContext()
+const INITIAL_STATE = {
+    users: [],
+};
 
 export const AuthContextProvider = ({ children }) => {
-    const userReducer = (state) =>{
-        
-    }
-    const [currentUser, setCurrentUser] = useState({})
-    const [state, dispatch] = useReducer(chatReducer, INITIAL_STATE)
+    const [currentUser, setCurrentUser] = useState([]);
+
+    const addUser = (userArray) => {
+        setCurrentUser([...currentUser, ...userArray]);
+    };
+
+
 
     return (
-        < AuthContext.Provider value={{ currentUser, dispatch  }} >
+        < AuthContext.Provider value={{ currentUser, addUser }} >
             {children}
         </AuthContext.Provider >
     )
